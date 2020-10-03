34429d942f32b20bf749dfee8f5f10ec634a9ffc
steve zhang <871506263@qq.com>
2020-10-03 21:31:59 +0800

    first commit

---
+++ src/components/Hi.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    I am Hi...
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Hi'
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    I am Home...
+    <div>
+      <router-link :to="{name: 'Hi'}">Hi</router-link>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+
+<style lang="less" scoped></style>
